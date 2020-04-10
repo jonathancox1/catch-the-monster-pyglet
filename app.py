cdb69e52bb8d1a37bf845b334dd41893e07043a4
Jonathan Cox <cox.jonathan@gmail.com>
2020-04-10 14:07:59 -0400

    changed sound for catching goblin

--- app.py
+++ app.py
@@ -80,7 +80,7 @@ def update(dt):
             score += 1
             score_label.text = f"Caught {score}"
 
-            gotcha_sound_effect = pyglet.media.load('./resources/bullet.wav', streaming=False)
+            gotcha_sound_effect = pyglet.media.load('./resources/win.wav', streaming=False)
             gotcha_sound_effect.play()
 
             # Add a new monster
